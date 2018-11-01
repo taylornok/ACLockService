5ddd8bbcfa21c0b35b1acfb009fd8d82bb0d49f9
taylornok <taylorowenskees@outlook.com>
2018-11-01 14:31:43 -0700

    adding more sequelize stuff

---
+++ models/contactModel.js
@@ -0,0 +1,31 @@
+module.exports = function (sequelize, DataTypes) {
+    const Contact = sequelize.define('Contact', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false, 
+            validate: {
+                len: [1]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            len: [10]
+
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: [10]
+        },
+        message: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: [50]
+        }
+    
+    });
+    
+    return Contact;
+
+};
